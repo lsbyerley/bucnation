e70212446d3318c05b23f30301aab664d48af1e7
lsbyerley <lsbyerley@gmail.com>
2022-08-04 08:51:50 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // trailingSlash: true,
   images: {
+    loader: 'akamai',
+    path: '',
     domains: [
       'ipfs.infura.io',
       'place-hold.it',
